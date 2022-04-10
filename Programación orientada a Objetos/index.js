448d63e4b17f49c37a7e77ab8ff7ead169b8d32c
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-10 01:12:01 -0300

    Programacion Orientada a Objetos - Clases - Heredando de una Clase

--- Programación orientada a Objetos/index.js
+++ Programación orientada a Objetos/index.js
@@ -11,7 +11,8 @@ class Tarea {
 
   //Nuevo método. Cuando pasas una función a una clase se convierte en un método
   mostrar() {
-    return `${this.nombre} tiene una prioridad ${this.prioridad}`;
+    // return `${this.nombre} tiene una prioridad ${this.prioridad}`;
+    console.log(`${this.nombre} tiene una prioridad ${this.prioridad}`);
   }
 }
 
@@ -21,8 +22,51 @@ let tarea2 = new Tarea("Preparar café", "Alta");
 let tarea3 = new Tarea("Pasear al perro", "Media");
 let tarea4 = new Tarea("Conocer a mis suegros", "Baja");
 
-console.log(tarea1);
-//Llamamos al método mostrar()
-console.log(tarea2.mostrar());
-console.log(tarea3);
-console.log(tarea4);
+// console.log(tarea1);
+// //Llamamos al método mostrar()
+// console.log(tarea2.mostrar());
+// console.log(tarea3);
+// console.log(tarea4);
+
+//HEREDANDO UNA CLASE:
+
+//Heredar una clase de otra en React. En React vamos a heredar nuestro código de la librería.
+//La principal ventaja de heredar es que puedes crear una clase padre y heredar todo eso a una clase distinta y solo realizar pequeños ajustes donde sea necesario.
+
+//Podés crear una clase en otro archivo
+//Con extends le decis la forma en la que la hereda y sabe a donde ir
+//La diferencia es que hereda el constructor, pero les estamos agregando un atributo extra.
+class ComprasPendientes extends Tarea {
+  //Le damos los dos argumentos anteriores y le sumamos la cantidad
+  constructor(nombre, prioridad, cantidad) {
+    //Se debe llamar al método super() cuando estás heredando una clase.Super hace referencia al atributo padre. JavaScript sabe que estas queriendo utilizar nombre y prioridad que son los mimos del constructor que está heredando.
+    super(nombre, prioridad);
+    this.cantidad = cantidad;
+  }
+
+  //Si queres reescribir mostrar() lo ponés acá, usas el mismo nombre para que sea el mismo método
+  // mostrar() {
+  //   return "hola";
+  // }
+
+  //Si queres agregarle una parte mas al método:
+  mostrar() {
+    //Hereda el texto anterior y le agrega otro mas
+    super.mostrar();
+    console.log(`y la cantidad de ${this.cantidad}`);
+  }
+
+  //Podes crear nuevos métodos. No limita si solo los tiene el padre o solo los tiene el hijo.
+  hola() {
+    return "hola";
+  }
+}
+
+//Compras
+// let compra1 = new ComprasPendientes("Jabon", "Urgente", 3);
+// console.log(compra1);
+
+//La ventaja es que una vez que heredás una clase dentro de otra podes utilizar los métodos también.
+let compra1 = new ComprasPendientes("Jabon", "Urgente", 3);
+console.log(compra1.mostrar());
+console.log(compra1.hola());
