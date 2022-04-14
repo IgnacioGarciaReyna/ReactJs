c2c90bfce2f2492f36a3b5312b4603f1783441ff
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-04-14 19:47:44 -0300

    Eventos en React

--- basicos/src/App.js
+++ basicos/src/App.js
@@ -1,14 +1,10 @@
-//El state es de las cosas mas importantes en React y buena parte de lo que permité que las apps de React sean tan rápidas.
-//En el state vas a colocar todo lo que va a reaccionar a ciertas acciones de los usuarios, por ejemplo los datos de un formulario cuando un usuario esté escribiendo, creamos un state que está leyendo mientras escriba. También un carrito de compras, que gracias al state se va actualizando cuando el usuario agrega un producto.
 
-//Vamos a importar useState
 import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Producto from "./components/Producto";
 
 function App() {
-  //Los objetos tienen ids para que cada componente tenga su key que lo haga único
   const [productos, guardarProductos] = useState([
     { id: 1, nombre: "Camisa ReactJS", precio: 50 },
     { id: 2, nombre: "Camisa VueJs", precio: 40 },
@@ -19,9 +15,6 @@ function App() {
 
   const fecha = new Date().getFullYear();
 
-  //Vamos a mostrar los productos.
-  //En js se utiliza el forEach pero en React se utiliza mas el map, mas que nada por que map devuelve un valor, forEach solo recorre.
-
   return (
     <Fragment>
       <Header titulo="Tienda virtual" />
@@ -29,10 +22,6 @@ function App() {
       <h1>Lista de productos</h1>
 
       {productos.map((producto) => (
-        //Return implicito
-        //Por cada producto escribe los que hay en Producto.js.
-        //Le pasamos los props
-        //Le pasamos la key para que sean únicos
         <Producto 
           key={producto.id} 
           producto={producto} />
--- basicos/src/components/Producto.js
+++ basicos/src/components/Producto.js
@@ -1,24 +1,33 @@
 import React from "react";
 
-//El snippet es "sfc"
-//Luego de que ya lo pasé en los props de app.js, voy a extraer el producto así ya tengo disponible la información.
 const Producto = ({ producto }) => {
-  //Se pueden extraer los valores
   const { nombre, precio, id } = producto;
 
-  return (
-    // <h1>desde producto</h1>
-    <div>
-      {
-        //Esto sería si no extraigo los valores
-        /* <h2>{producto.nombre}</h2> */
-      }
+  //Agregar producto al carrito
+  //Como solo se pasa un parametro, se pueden eliminar los parentesis para que el código sea un poco mas corto.
+  const seleccionarProducto = id => {
+    console.log("Comprando...", id);
+  };
 
-      {/* Esto es extrayendo los valores */}
+  return (
+    <div>
       <h2>{nombre}</h2>
       <p>$ {precio}</p>
+      {/* Agregamos un boton "Agregar al carrito" 
+          React maneja lo botones de forma distinta a JS tradicional.
+          En JS le colocas un id y escribirias un getById o QuerySelector y le agregarías un eventListenner
+          En React ya tenes una gran cantidad de eventos. Se agrega el prefijo on.
+          Al onClik le podes agregar la palabra function directamente entre llaves, sin embargo, vamos a esperar a que el usuario dé click, por lo tanto tiene que ser una arrow function. Si colocas "function" y lo mandas a llamar es prácticamente un llamado a una función, se va a ejecutar automáticamente.*/}
+      <button type="button" onClick={() => seleccionarProducto(id)}>
+        Comprar
+      </button>
     </div>
   );
 };
 
 export default Producto;
+
+
+//Lo importante es como React maneja los eventos.
+//Se agrega un on y el nombre del evento, en este caso un click. 
+//Usualmente será con una arrow function o con una función directamente.
